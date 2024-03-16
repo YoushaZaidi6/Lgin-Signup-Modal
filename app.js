1217222c2fbd365c8db26419f859bf08916fe821
YoushaZaidi6 <youshazaidi6@gmail.com>
2024-03-16 16:49:22 +0500

    title

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 function signupToggle(){
     var container = document.querySelector('.container');
     container.classList.toggle('active');
-    var signupPopup = document.querySelector('.signup-form');
-    var loginPopup = document.querySelector('.login-form');
-    
-    if (loginPopup.classList.contains('active')) {
-        loginPopup.classList.remove('active');
-    }
-
-    signupPopup.classList.toggle('active');
+    var popup = document.querySelector('.signup-form');
+    popup.classList.toggle('active');
 }
 
 function loginToggle(){
     var container = document.querySelector('.container');
     container.classList.toggle('active');
-    var signupPopup = document.querySelector('.signup-form');
-    var loginPopup = document.querySelector('.login-form');
-
-    if (signupPopup.classList.contains('active')) {
-        signupPopup.classList.remove('active');
-    }
-
-    loginPopup.classList.toggle('active');
+    var popup = document.querySelector('.login-form');
+    popup.classList.toggle('active');
 }
+
 
